973d0eec5c020e16da4736ec3c29e7af93b28852
Manish <manishmanjrekar@outlook.com>
2019-10-06 14:44:25 +0530

    added the sample get,post call from web api and test its working

---
+++ src/Models/VendorsModel.ts
@@ -0,0 +1,8 @@
+export class VendorsModel{
+    name:string;
+    address:string;
+    city:string;
+    referredBy:string;
+    mobile:string;
+    alternateMobile:string;
+  }
--- src/app/components/vendor/vendor.component.ts
+++ src/app/components/vendor/vendor.component.ts
@@ -1,19 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { VendorsModel } from 'Models/VendorsModel';
+
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type':  'application/json'
+    //'Authorization': 'my-auth-token'
+  })
+};
 
 @Component({
   selector: 'app-vendor',
   templateUrl: './vendor.component.html',
   styleUrls: ['./vendor.component.css']
 })
-export class VendorComponent implements OnInit {
 
+
+
+export class VendorComponent implements OnInit 
+
+
+{
+  
   vendorForm: FormGroup;
   submitted = false;
+  headers: Headers;
 
   mobilePattern = "^[6-9][0-9]{9}$";
-
+  test = new VendorsModel();
+  
   constructor(private formBuilder: FormBuilder,private httpClient: HttpClient) { }
 
   ngOnInit() {
@@ -29,11 +45,16 @@ export class VendorComponent implements OnInit {
 
    
   }
+
+  
   public getContacts(){
-    var obj = this.httpClient.get<string[]>("http://localhost:54436/api/Values");
-    console.log(obj[0]);
-    alert(obj);
-}
+    
+
+    // this.httpClient.get('http://localhost:54436/api/Vendor').subscribe(data => {
+    //   console.log(data);
+    // });
+  }
+
   // convenience getter for easy access to form fields
  get f() { return this.vendorForm.controls; }
 
@@ -46,7 +67,24 @@ export class VendorComponent implements OnInit {
 
 
   }
-
+ var data =JSON.stringify(this.vendorForm.value);
   alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.vendorForm.value))
+ var url='http://localhost:54436/api/Vendor/AddVendor';
+
+
+     
+  this.httpClient.post(url, data,httpOptions).subscribe(
+    res => {
+      this.test.name =res["name"];
+      console.log('res'+ this.test.name);
+    },
+    (err: HttpErrorResponse) => {
+      console.log(err.error);
+      console.log(err.name);
+      console.log(err.message);
+      console.log(err.status);
+    }
+  );
+ 
  }
 }
